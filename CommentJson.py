84b5a7b2533d6cd7154f0e6586cb744d28f94672
Jaime Du <duyangeu@gmail.com>
2019-08-03 09:26:07 +0800

    get comment paginate

--- CommentJson.py
+++ CommentJson.py
@@ -16,15 +16,17 @@ def find_comment_by_id(product_id):
     driver = webdriver.PhantomJS(executable_path="/usr/local/bin/phantomjs")
     color = []
     size = []
-    jd_url = 'https://item.jd.com/27344400591.html#comment'
+    jd_url = 'https://sclub.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv48&productId=27344400591&score=0&sortType=5&page=2&pageSize=10&isShadowSku=0&rid=0&fold=1'
     driver.get(jd_url)
-    time.sleep(30)
-
+    time.sleep(60)
+    with open('comment_json.txt', "w+") as f:
+        f.write(driver.page_source)
+    return
     for i in range(2, 20):
         try:
             #path = ".//div[@class ='ui-page']/a[@ rel='{}']".format(str(i))
             path = ".//div[@class ='ui-page']/a[@ rel='2']"
-            driver.find_element_by_xpath(".//div[@class ='ui-page']/a[@ rel='3']").click()
+            driver.find_element_by_xpath(".//div[@class ='ui-page']/a[@ rel='2']").click()
             time.sleep(60)
             with open('comment.txt', "w+") as f:
                 f.write(driver.page_source)

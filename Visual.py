05673cc0b9e263a8c39cc2ea246ebe08f85857d1
Jaime Du <duyangeu@gmail.com>
2019-08-06 16:51:45 +0800

    install matplotlib for data visualization

--- Visual.py
+++ Visual.py
@@ -1,1 +1,9 @@
 import matplotlib.pyplot as plt
+
+labels = 'Python', 'C++', 'Java'
+sizes = [215, 130, 245]
+colors = ['gold', 'yellowgreen', 'lightcoral']
+explode = (0.1, 0, 0)
+plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+plt.axis('equal')
+plt.savefig('./public/charts/pie.png', bbox_inches='tight')
